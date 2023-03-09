75b03a221ee0c41c25c2623d67178c503ece5838
Agustin Baza <agustinbaza10@gmail.com>
2023-03-09 01:20:06 -0300

    agregue y controle el funcionamiento del ejercicio 3

--- js/EJ-1.js
+++ js/EJ-1.js
@@ -3,14 +3,14 @@
 let edad = prompt("Ingrese la edad");
 
 switch (true) {
-    case edad < 18: 
+  case edad < 18:
     document.write("No puede conducir");
     break;
 
-    case edad>=18 && edad < 101:
+  case edad >= 18 && edad < 101:
     document.write("Puede conducir");
     break;
 
-    default: 
+  default:
     document.write("No se ingreso un caracter valido");
 }
