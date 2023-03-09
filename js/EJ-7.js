53b1b9225402c7301a86c47f98a30376e470be98
agustinbaza <124207542+agustinbaza@users.noreply.github.com>
2023-03-09 20:41:34 -0300

    Update EJ-7.js

--- js/EJ-7.js
+++ js/EJ-7.js
@@ -2,6 +2,10 @@
 
 
 
-for(let contador = prompt("INGRESE UN NÚMERO NO MAYOR DE 50"); contador<51; contador-- ){
+for(
+    let contador = prompt("INGRESE UN NÚMERO NO MAYOR DE 50");
+    contador<51;
+    contador-- 
+){
     document.write (String(contador).repeat(contador)+'<br>');
 }
